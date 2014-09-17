1ca5e08564babca5668c3867805180c08a868c87
IamAlchemist <babyfat.candy@gmail.com>
2014-09-17 11:36:44 +0800

    add search button in action bar

--- Demo/app/src/main/java/com/tiny/wizard/demo/MainActivity.java
+++ Demo/app/src/main/java/com/tiny/wizard/demo/MainActivity.java
@@ -3,7 +3,6 @@ package com.tiny.wizard.demo;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -30,15 +29,14 @@ public class MainActivity extends Activity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            Log.i("msg", "setting action pressed");
-            return true;
+        switch (item.getItemId()){
+            case R.id.action_settings:
+                return true;
+            case R.id.action_search:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
-        return super.onOptionsItemSelected(item);
     }
 
     public void sendMessage(View view){
