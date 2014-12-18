c29349ff1f8541387d0b78fd25d35c99622a67ae
IamAlchemist <babyfat.candy@gmail.com>
2014-12-18 15:03:00 +0800

    implement section list view

--- UIExample/app/src/main/java/com/tiny/wizard/uiexample/SectionListActivity.java
+++ UIExample/app/src/main/java/com/tiny/wizard/uiexample/SectionListActivity.java
@@ -2,12 +2,78 @@ package com.tiny.wizard.uiexample;
 // Created by wizard on 12/17/14.
 
 import android.app.ListActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class SectionListActivity extends ListActivity{
+
+    private ArrayList<String> mydata1 = new ArrayList<String>();
+    private ArrayList<String> mydata2 = new ArrayList<String>();
+
+    private ArrayList< ArrayList<String>> mydata  = new ArrayList<ArrayList<String>>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_section_list);
+
+        generateData();
+
+        setListAdapter(new MyAdapter(this));
+    }
+
+    private void generateData() {
+        for(int i = 0; i < 20; ++i){
+            mydata1.add("item" + i);
+        }
+
+        for(int i = 20; i > 0; --i){
+            mydata2.add("item" + i);
+        }
+
+        mydata.add(mydata1);
+        mydata.add(mydata2);
+    }
+
+
+    private class MyAdapter extends SectionListAdapter{
+
+        public MyAdapter(Context context) {
+            super(context);
+        }
+
+        @Override
+        public int numberOfRowsInSection(int section) {
+            return mydata.get(section).size();
+        }
+
+        @Override
+        public int numberOfSections() {
+            return mydata.size();
+        }
+
+        @Override
+        public View getViewForIndexPath(IndexPath indexPath, View view, ViewGroup viewGroup) {
+            if(view == null){
+                LayoutInflater inflater = LayoutInflater.from(SectionListActivity.this);
+                view = inflater.inflate(R.layout.row_section_list, viewGroup, false);
+            }
+
+            TextView textView = (TextView)view.findViewById(R.id.row_section_list_text);
+            textView.setText(mydata.get(indexPath.section).get(indexPath.row));
+
+            return view;
+        }
+
+        @Override
+        public String titleForHeaderInSection(int section){
+            return "section" + section;
+        }
     }
 }
--- UIExample/app/src/main/java/com/tiny/wizard/uiexample/SectionListAdapter.java
+++ UIExample/app/src/main/java/com/tiny/wizard/uiexample/SectionListAdapter.java
@@ -1,28 +1,62 @@
 package com.tiny.wizard.uiexample;
-// Created by wizard on 12/17/14.
 
+// Created by wizard on 12/17/14.
+// Support single type of cell view and single type of header - v0.1
+
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
 
-import java.util.ArrayList;
+public abstract class SectionListAdapter extends BaseAdapter {
+    private Context context;
 
-public abstract class SectionListAdapter<T> extends BaseAdapter {
+    public SectionListAdapter(Context context){
+        this.context = context;
+    }
+
+    public final int VIEW_TYPE_CELL = 0;
+    public final int VIEW_TYPE_HEADER = 1;
+
+    private class InternalIndexPath{
+        IndexPath indexPath;
+        int type;
+        InternalIndexPath(){
+            indexPath = new IndexPath();
+            type = VIEW_TYPE_CELL;
+        }
+    }
+
+    public class IndexPath{
+        int row;
+        int section;
+    }
 
     public abstract int numberOfRowsInSection(int section);
     public abstract int numberOfSections();
+    public abstract View getViewForIndexPath(IndexPath indexPath, View view, ViewGroup viewGroup);
 
-    public abstract int resourceIdForRowInSection(int row, int section);
-    public abstract int resourceIdForSection(int row);
+    @Override
+    public int getViewTypeCount(){
+        return 2;
+    }
 
-    public int heightForRowInSection(int row, int section){
+    @Override
+    public int getItemViewType(int position){
+        InternalIndexPath internalIndexPath = indexPathFromPosition(position);
+        return internalIndexPath.type;
+    }
+
+    public int heightForRow(IndexPath indexPath){
         return 44;
     }
     public int heightForHeaderInSection(int section) {
-        return 0;
+        return 44;
     }
     public int heightForFooterInSection(int section){
-        return 0;
+        return 44;
     }
 
     public String titleForHeaderInSection(int section){
@@ -32,15 +66,23 @@ public abstract class SectionListAdapter<T> extends BaseAdapter {
         return "";
     }
 
-
-
-
-    ArrayList<T> data = new ArrayList<T>();
-    ArrayList<String> sections = new ArrayList<String>();
+    public View getViewForSectionHeader(int section, View view, ViewGroup viewGroup){
+        return null;
+    }
 
     @Override
     public int getCount() {
-        return 0;
+        int allSize = 0;
+        int numberOfSections = numberOfSections();
+        for(int i = 0; i < numberOfSections; ++i){
+            allSize += numberOfRowsInSection(i);
+        }
+
+        if(numberOfSections > 1){
+            allSize += numberOfSections;
+        }
+
+        return allSize;
     }
 
     @Override
@@ -50,11 +92,74 @@ public abstract class SectionListAdapter<T> extends BaseAdapter {
 
     @Override
     public long getItemId(int i) {
-        return 0;
+        return i;
     }
 
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
-        return null;
+        InternalIndexPath internalIndexPath = indexPathFromPosition(i);
+
+        View resultView = view;
+        switch (internalIndexPath.type){
+            case VIEW_TYPE_CELL:
+                resultView = getViewForIndexPath(internalIndexPath.indexPath, view, viewGroup);
+                break;
+            case VIEW_TYPE_HEADER:
+                resultView = viewForSectionHeader(internalIndexPath.indexPath.section, view, viewGroup);
+                break;
+        }
+
+        return resultView;
     }
+
+    private View viewForSectionHeader(int section, View view, ViewGroup viewGroup) {
+        View headerView = getViewForSectionHeader(section, view, viewGroup);
+        if(headerView != null)
+            return headerView;
+
+        if(view != null)
+            headerView = view;
+        else
+            headerView = LayoutInflater.from(context).inflate(android.R.layout.simple_list_item_1, viewGroup, false);
+
+        TextView textView = (TextView)headerView.findViewById(android.R.id.text1);
+        textView.setText(titleForHeaderInSection(section));
+
+        return headerView;
+    }
+
+    private InternalIndexPath indexPathFromPosition(int position){
+        InternalIndexPath internalIndexPath = new InternalIndexPath();
+        int numberOfSections = numberOfSections();
+
+        if(numberOfSections == 1){
+            internalIndexPath.indexPath.row = position;
+            internalIndexPath.indexPath.section = 0;
+            internalIndexPath.type = VIEW_TYPE_CELL;
+        }
+        else{
+            for(int sectionIndex = 0; sectionIndex < numberOfSections; ++sectionIndex){
+                int numberOfRowsInSection = numberOfRowsInSection(sectionIndex);
+
+                if(position < numberOfRowsInSection + 1){
+                    internalIndexPath.indexPath.section = sectionIndex;
+                    internalIndexPath.indexPath.row = position - 1;
+                    internalIndexPath.type = getTypeOfIndexPath(internalIndexPath.indexPath);
+            }
+                else{
+                    position -= numberOfRowsInSection + 1;
+                }
+            }
+        }
+
+        return internalIndexPath;
+    }
+
+    private int getTypeOfIndexPath(IndexPath indexPath) {
+        if(indexPath.row == -1)
+            return VIEW_TYPE_HEADER;
+        return VIEW_TYPE_CELL;
+    }
+
+
 }
