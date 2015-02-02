40a187e74af311fe28d46460da5b52fe1b7a2352
IamAlchemist <babyfat.candy@gmail.com>
2015-02-02 15:21:58 +0800

    minor change

---
+++ SampleBasic/app/src/main/java/com/tiny/wizard/samplebasic/BindServiceActivity.java
@@ -0,0 +1,12 @@
+package com.tiny.wizard.samplebasic;
+// Created by wizard on 2/2/15.
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BindServiceActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+    }
+}
--- SampleBasic/app/src/main/java/com/tiny/wizard/samplebasic/MainActivity.java
+++ SampleBasic/app/src/main/java/com/tiny/wizard/samplebasic/MainActivity.java
@@ -18,39 +18,82 @@ import java.util.Map;
 
 public class MainActivity extends ListActivity {
 
+    private static final String LABEL_PREFIX = "com.tiny.wizard.samplebasic.main_activity.label_prefix";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setListAdapter(new SimpleAdapter(this, getData(),
+
+        String prefix = getIntent().getStringExtra(LABEL_PREFIX);
+
+        setListAdapter(new SimpleAdapter(this, getData(prefix),
                 android.R.layout.simple_list_item_1, new String[] {"title"},
                 new int[] { android.R.id.text1 }));
     }
 
-    private List<Map<String, Object>> getData() {
+    private List<Map<String, Object>> getData(String prefix) {
         List<Map<String, Object>> myData = new ArrayList<>();
 
         Intent intentWithCustomAction = new Intent();
         intentWithCustomAction.setAction(getResources().getString(R.string.sample_basic_action));
-
         PackageManager pm = getPackageManager();
         List<ResolveInfo> resolveInfos = pm.queryIntentActivities(intentWithCustomAction, 0);
+
+        HashMap<String, Boolean> nextLabelRecord = new HashMap<>();
 
         for(ResolveInfo resolveInfo : resolveInfos){
             CharSequence labelSequence = resolveInfo.loadLabel(pm);
             String label = labelSequence == null ?
                     resolveInfo.activityInfo.name : labelSequence.toString();
+            prefix = prefix == null ? "" : prefix;
+            if(prefix.length() == 0 || label.startsWith(prefix)) {
+                String nextLabel = getNextLabel(prefix, label);
+                Intent intent = makeIntent(prefix, label, resolveInfo);
 
-            Map<String, Object> entries = new HashMap<>();
-            Intent intent = new Intent();
-            intent.setClassName(resolveInfo.activityInfo.applicationInfo.packageName,
-                    resolveInfo.activityInfo.name);
-            entries.put("title", label);
-            entries.put("intent", intent);
+                if(nextLabelRecord.get(nextLabel) == null){
+                    nextLabelRecord.put(nextLabel, true);
 
-            myData.add(entries);
+                    Map<String, Object> entries = new HashMap<>();
+                    entries.put("title", nextLabel);
+                    entries.put("intent", intent);
+
+                    myData.add(entries);
+                }
+            }
         }
 
         return myData;
+    }
+
+    private Intent makeIntent(String prefix, String label, ResolveInfo resolveInfo) {
+        Intent intent = new Intent();
+
+        String[] prefixes = prefix.split("/");
+        String[] labels = label.split("/");
+
+        if((prefix.length() == 0 && labels.length == 1) ||
+                (prefix.length() != 0 && prefixes.length == labels.length - 1)){
+            intent.setClassName(resolveInfo.activityInfo.applicationInfo.packageName,
+                    resolveInfo.activityInfo.name);
+        }
+        else{
+            prefix = prefix.length() == 0 ? labels[0] : prefix + labels[prefixes.length];
+
+            intent.putExtra(LABEL_PREFIX, prefix + "/");
+            intent.setClass(this, MainActivity.class);
+        }
+
+        return intent;
+    }
+
+    private String getNextLabel(String prefix, String label) {
+        String[] prefixes = prefix.split("/");
+        String[] labels = label.split("/");
+
+        if(prefix.length() == 0)
+            return labels[0];
+
+        return labels[prefixes.length];
     }
 
     private void startActivity(Class<? extends Activity> activityClass){
